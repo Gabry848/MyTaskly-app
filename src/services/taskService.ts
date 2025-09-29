a552e19d2110d0f8863e603a9e04fd072f46701b
Gabry848 <famiglia.cipriani3@gmail.com>
2025-09-29 15:23:39 +0200

    fix(taskService): restore cache check in getTasks function

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -77,8 +77,7 @@ export async function getTasks(category_name?: string, useCache: boolean = true)
       }
       
       const cachedTasks = await getServices().cacheService.getCachedTasks();
-      //if (cachedTasks.length > 0) {
-      if (false) {
+      if (cachedTasks.length > 0) {
         console.log('[TASK_SERVICE] Usando dati dalla cache');
         
         // Filtra per categoria se specificata
